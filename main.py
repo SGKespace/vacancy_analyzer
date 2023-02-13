69405d01ace54fde93293bdbef4f581f9a478200
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-13 20:03:54 +0400

    количество вакансий по языкам программирования

--- main.py
+++ main.py
@@ -5,38 +5,40 @@ from itertools import count
 
 
 def main():
-    hh_vacancies = get_hh_vacancies()
-    city_ = 'Москва'
-    all_Moscow_vacancies=[]
-    for page_number, current_page in enumerate(hh_vacancies):
-        for vacance in current_page['items']:
-            if vacance['area']['name'] == city_:
-                all_Moscow_vacancies.append(vacance)
-                print(vacance)
+    programming_languages = {'Java', 'Javascript', 'Python', 'Ruby', 'PHP', 'C++', 'C#'}
+    list_count_programming_language = programming_language_count(programming_languages)
+    print(list_count_programming_language)
 
-    current_month = datetime.date.today().month
-    current_month_Moscow_vacancies = []
-    for vacance in all_Moscow_vacancies:
-        if current_month == parse(vacance['published_at']).month:
-            current_month_Moscow_vacancies.append(vacance)
-    return all_Moscow_vacancies, current_month_Moscow_vacancies
+def programming_language_count(programming_languages):
+    language_counts = {}
+    for programming_language in programming_languages:  # Бежим по списку языков
+        search_text = f'Программист {programming_language}'
+        count = 0
+        town = 1
+        url = 'https://api.hh.ru/vacancies'
+        params = {'text': f'{search_text}', 'search_field': 'name', 'area': town}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        programming_language_vacancies = response.json()
+        language_counts[f'{programming_language}'] = programming_language_vacancies['found']
+    return language_counts
 
 
-def get_hh_vacancies(town=1):
+def get_hh_vacancies(search_text, town=1):  # Все вакансии Москвы
     url = 'https://api.hh.ru/vacancies'
-    search_text = 'Программист'
     all_vacancies = []
-
     for page in count(0):
         params = {'text': f'{search_text}', 'search_field': 'name', 'area': town, 'page': page}
         response = requests.get(url, params=params)
         response.raise_for_status()
         vacancies = response.json()
+        all_vacancies.append(vacancies)
         if page >= vacancies['pages'] - 1:
             break
-        all_vacancies.append(vacancies)
-
     return all_vacancies
+
+
+
 
 
 if __name__ == '__main__':
